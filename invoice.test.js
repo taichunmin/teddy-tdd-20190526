bc7d901c282c94ea69ac6dc1fdb7f18324156948
taichunmin <taichunmin@gmail.com>
2019-05-26 14:45:14 +0800

    test fail

--- invoice.test.js
+++ invoice.test.js
@@ -13,3 +13,15 @@ test('用含稅價格計算稅金', async () => {
   expect(invoice.tax).toEqual(810)
   expect(invoice.untaxedPrice).toEqual(16190)
 })
+
+test('用含稅價格計算稅金，需要四捨五入', async () => {
+  // act
+  let invoice = new Invoice(VAT_RATE)
+  invoice.setTaxIncludedPrice(99)
+
+  // assert
+  expect(invoice.rate).toEqual(VAT_RATE)
+  expect(invoice.taxIncludedPrice).toEqual(99)
+  expect(invoice.tax).toEqual(5)
+  expect(invoice.untaxedPrice).toEqual(94)
+})
